14fe000545a15a6d192288752ff95b28daea9aea
peresamartin <79202913+peresamartin@users.noreply.github.com>
2023-09-18 17:00:25 +0200

    website

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate')
 
 /////////////////////////////////////////////////////////////////////////////////////////
 // FILES
@@ -33,22 +35,44 @@ const url = require('url');
 /////////////////////////////////////////////////////////////////////////////////////////
 // SERVER
 
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const server = http.createServer((req, res) => {
-    const pathName = req.url;
+const slugs = dataObj.map(el => 
+    slugify(el.productName, {lower: true})
+);
 
-    if(pathName === '/' || pathName === '/overview'){
-        res.end('This is the overview');
+console.log(slugs)
+
+const server = http.createServer((req, res) => {
+    
+    const { query, pathname } = url.parse(req.url, true);
+
+    // Overview page
+    if(pathname === '/' || pathname === '/overview'){
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        
+        res.end(output);
     }
-    else if(pathName === '/api'){
+    // API
+    else if(pathname === '/api'){
         res.writeHead(200, {'Content-type': 'application/json'})
         res.end(data);
+    } 
+    // Product page
+    else if(pathname === '/product'){
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const product =dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
     }
-    else if(pathName === '/product'){
-        res.end('This is the product');
-    }
+    // Not found
     else{
         res.writeHead(404, {
             'Content-type': 'text/html',
